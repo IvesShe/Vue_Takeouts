a72d37d306107bc66e9616aa4e1886970967cf98
IvesShe <ivesshe@gmail.com>
2021-11-14 12:17:42 +0800

    update

---
+++ vue_takeouts/src/pages/Order/Order.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="order">
+    <header-top title="訂單列表"></header-top>    
+    <section class="order_no_login">
+      <img src="./images/person.png">
+      <h3>登入後查看外賣訂單</h3>
+      <button>立即登入</button>
+    </section>
+  </div>
+</template>
+
+<script>
+import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
+export default {
+  components:{
+    HeaderTop
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.stylus"
+  .order  //訂單
+    width 100%    
+    .order_no_login
+      padding-top 140px
+      width 60%
+      margin 0 auto
+      text-align center
+      >img
+        display block 
+        width 100%
+        height 30%
+      >h3
+        padding 10px 0
+        font-size 17px
+        color #6a6a6a
+      >button
+        display inline-block
+        background #02a774
+        font-size 14px
+        color #fff
+        border 0
+        outline none
+        border-radius 5px
+        padding 10px 20px
+  
+</style>
